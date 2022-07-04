a04a90cc3ca811708f61759bb689dd7c6f98c569
Valentin Churavy <v.churavy@gmail.com>
2022-07-04 16:27:58 -0400

    trigger follow on

--- process.sh
+++ process.sh
@@ -4,3 +4,11 @@ echo ${VIDEO_URL}
 
 echo "--- Upload the processed video"
 # buildkite-agent artifact upload "test/results.json"
+
+echo "--- Trigger Zapier pipeline"
+curl -X POST "https://hooks.zapier.com/hooks/catch/12874263/bw07v2l/" \
+   -d '{
+       "ID": "${ID}",
+       "BUILDKITE_BUILD_NUMBER": "${BUILDKITE_BUILD_NUMBER}",
+       "BUILDKITE_JOB_ID": "${BUILDKITE_JOB_ID}"
+    }'
