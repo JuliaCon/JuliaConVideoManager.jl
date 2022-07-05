4b0a705295339a5a5140e313bfb7953ae121a4b5
Valentin Churavy <v.churavy@gmail.com>
2022-07-04 21:18:15 -0400

    Use Julia script to process

--- process.sh
+++ process.sh
@@ -1,19 +1,24 @@
+echo "-- Download the intro"
+
+# curl "https://juliacon2020-uploads.s3.us-east-2.amazonaws.com/public/UNKNWN%3Aadditionalvideomaterial%3Aanimation-lo-res-400.mov" -o "intro.mov"
+
 echo "-- Download video"
 
 # Does ffmpeg care about the file ending?
-curl "${VIDEO_URL}" --output "${ID}.mp4"
+# curl "${VIDEO_URL}" --output "${ID}.mp4"
 
 echo "-- Process the video"
-
-# TODO
+mkdir -p out
+julia --project=. -e 'using Pkg; Pkg.instantiate()'
+julia --project=. processing.jl "intro.mov" "${ID}.mp4"
 
 echo "--- Upload the processed video"
-buildkite-agent artifact upload "${ID}.mp4"
+# buildkite-agent artifact upload "${ID}.mp4"
 
-echo "--- Trigger Zapier pipeline"
-curl -X POST "https://hooks.zapier.com/hooks/catch/12874263/bw07v2l/" \
-   -d '{
-       "ID": "'${ID}'",
-       "BUILDKITE_BUILD_NUMBER": "'${BUILDKITE_BUILD_NUMBER}'",
-       "BUILDKITE_JOB_ID": "'${BUILDKITE_JOB_ID}'"
-    }'
+# echo "--- Trigger Zapier pipeline"
+# curl -X POST "https://hooks.zapier.com/hooks/catch/12874263/bw07v2l/" \
+#    -d '{
+#        "ID": "'${ID}'",
+#        "BUILDKITE_BUILD_NUMBER": "'${BUILDKITE_BUILD_NUMBER}'",
+#        "BUILDKITE_JOB_ID": "'${BUILDKITE_JOB_ID}'"
+#     }'
--- processing.jl
+++ processing.jl
@@ -1,27 +1,40 @@
- using DaggerGPU
- using FFMPEG
- using Distributed
- using JuliaConVideoManager
- # Hello world with FFMPEG
- #FFMPEG.exe("-version")
- postervideos = pkgdir(JuliaConVideoManager, "src/postervideos")
-  # Let's try to run the bare bash script with no GPUs
- files = readdir(postervideos)                                                                                                                                                              19 intro = files[files .== "intro.mp4"] |> only
- # Ugly to have such stateful transformations but meh
- files = filter(x -> !startswith("intro", x), files)
- @show files[1]
- @show intro
- 
-function recipe(target, intro)
-     """
-     ffmpeg -i $intro -i "$target"  -filter_complex \
-     "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1,setsar=1,fps=30,format=yuv420p[v0];
-     [1:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1,setsar=1,fps=30,format=yuv420p[v1];
-     [v0][0:a][v1][1:a]concat=n=2:v=1:a=1[v][a]" \
-     -map "[v]" -map "[a]" -c:v libx264 -c:a aac -movflags +faststart "processed/$target"
-     """
-end
+using FFMPEG
+using JSON
 
-@time for f in files
-     FFMPEG.exe(recipe(f, intro))
-end
+@assert length(ARGS) == 2
+const preroll = ARGS[1]
+const file = ARGS[2]
+
+# 1. Query properties of file
+
+json = join(FFMPEG.exe(`-v error -show_entries stream=width,height -of json $(file)`, command=FFMPEG.ffprobe, collect=true), "\n")
+properties = JSON.Parser.parse(json)
+streams = properties["streams"]
+
+filter!(!isempty, streams)
+sizes = map(stream -> (stream["width"], stream["height"]), streams)
+
+unique!(sizes)
+width, height = only(sizes)
+
+args = ```
+-i $(preroll)
+-i $(file)
+-filter_complex 
+"[0:v]scale=$(width):$(height):force_original_aspect_ratio=decrease,pad=$(width):$(height):-1:-1,setsar=1,fps=30,format=yuv420p[v0]; \
+ [1:v]setsar=1,fps=30,format=yuv420p[v1];
+  [v0][0:a][v1][1:a]concat=n=2:v=1:a=1[v][a]"
+-map "[v]"
+-map "[a]"
+-c:v libx264
+-preset slow
+-crf 18
+-c:a aac
+-b:a 192k
+-movflags +faststart
+out/$(file)
+```
+
+@show args
+FFMPEG.exe(args)
+
