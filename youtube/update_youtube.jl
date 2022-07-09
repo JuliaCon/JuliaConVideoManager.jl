e54e81dabe35915276e82735af61a540f4cd9087
Valentin Churavy <v.churavy@gmail.com>
2022-07-08 21:50:15 -0400

    Improve update youtube

---
+++ youtube/update_youtube.jl
@@ -0,0 +1,79 @@
+include("../bk-downloader/airtable.jl")
+include("Youtube.jl")
+
+client = load_or_obtain()
+refresh!(client)
+
+const baseId = "appAeQFpa0ywisJKd"
+const sheet1Id = "tblube5UaXuYqPzDq"
+
+airtable_get(;query=Dict{Symbol,String}()) = Airtable.get(baseId, sheet1Id; query)
+
+table = airtable_get()
+
+function format_description(desc)
+    """
+    $(desc)
+
+    For more info on the Julia Programming Language, follow us on Twitter: https://twitter.com/JuliaLanguage and consider sponsoring us on GitHub: https://github.com/sponsors/JuliaLang
+
+    00:00 Welcome!
+    00:10 Help us add time stamps or captions to this video! See the description for details.
+
+    Want to help add timestamps to our YouTube videos to help with discoverability? Find out more here: https://github.com/JuliaCommunity/YouTubeVideoTimestamps 
+
+    Interested in improving the auto generated captions? Get involved here: https://github.com/JuliaCommunity/YouTubeVideoSubtitles
+    """
+end
+
+
+function format_title(title, author; short =false)
+    if !short
+        return "$title | $author | JuliaCon 2022"
+    else
+        return "$title | $author"
+    end
+end
+
+function update_metadata(client, table)
+    for record in table
+        fields = record[:fields]
+        pretalx_id = fields[Symbol("Pretalx ID")]
+        yt_id = Base.get(fields, Symbol("Youtube ID"), nothing)
+
+        if yt_id === nothing
+            @warn "Youtube ID missing" pretalx_id
+            continue
+        end
+
+        title = fields[Symbol("Proposal title")]
+        author = fields[Symbol("Speaker names")]
+        abstract = fields[Symbol("Abstract")]
+
+        yt_title = format_title(title, author)
+        if length(yt_title) > 100
+            yt_title = format_title(title, author, short=true)
+        end
+        if length(yt_title) > 100
+            @error "Title to long" pretalx_id yt_title
+            continue
+        end
+        yt_desc = format_description(abstract)
+
+        parts = Dict(
+            :snippet => Dict(
+                :title => yt_title,
+                :description => yt_desc,
+                :categoryId => "28",
+            ),
+        )
+
+        resp = video_update!(client, yt_id, parts)
+        if resp.status != 200
+            @warn "Update failed" pretalx_id resp
+        end
+    end
+end
+
+update_metadata(client, table)
+
